222c29997d0e4e45d55e356f86e71b77a2935699
zuneem tamrakar <zuneemtamrakar@gmail.com>
2022-07-07 11:04:58 -0500

    place the order automatically using selenium

--- project.py
+++ project.py
@@ -2,6 +2,9 @@ import glob
 import os
 import pandas as pd
 import csv 
+import time
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
 
 
 def main():
@@ -9,6 +12,8 @@ def main():
     combine_files(x)
     new_file = 'order.csv'
     total_units(new_file)
+    sales = 'sales.csv'
+    place_order(get_var(sales))
 
 # go to the Directory and take seven most recent sales reports 
 
@@ -55,6 +60,107 @@ def get_order_quantity(quantity,pack):
         return quantity // pack
     else :
         return (quantity // pack ) +1
+
+# once we have the quantity that we need to order, we can how automate the
+# order process using selenium
+
+def place_order(new_dict):
+    
+    eleven_x_fourteen = get_order_quantity(new_dict['11x14'],7)
+    eight_x_ten = get_order_quantity(new_dict['8x10'],10)
+    sixteen_x_twenty = get_order_quantity(new_dict['16x20'],5)
+    twelve_x_twentyfour = get_order_quantity(new_dict['12x24'],3)
+    eighteen_x_twentyfour = get_order_quantity(new_dict['18x24'],3)
+    twentyfour_x_thirtysix = get_order_quantity(new_dict['24x36'],1)
+    
+    # print(eleven_x_fourteen)
+    # print(eight_x_ten)
+    # print(sixteen_x_twenty)
+    # print(twelve_x_twentyfour)
+    # print(eighteen_x_twentyfour)
+    # print(twentyfour_x_thirtysix)
+    
+
+    browser = wd.Chrome('/Users/zuneemtamrakar/bin/chromedriver')
+    browser.implicitly_wait(20)
+
+    browser.get('https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Super-Value-Blank-Canvas-Set---11%22-x-14%22/p/80872879')
+    browser.implicitly_wait(25)
+
+    quan_11x14 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_11x14.clear()
+    time.sleep(5)
+    type(quan_11x14)
+    quan_11x14.send_keys(eleven_x_fourteen)
+    time.sleep(13)
+    add1 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add1.click()
+    time.sleep(8)
+
+    browser.get('https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Super-Value-Blank-Canvas-Set---8%22-x-10%22/p/80872876')
+    quan_8x10 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_8x10.clear()
+    time.sleep(4)
+    type(quan_8x10)
+    quan_8x10.send_keys(eight_x_ten)
+    time.sleep(9)
+    add2 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add2.click()
+    time.sleep(7)
+
+    browser.get('https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Super-Value-Blank-Canvas-Set---16%22-x-20%22/p/80872705')
+    quan_16x20 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_16x20.clear()
+    time.sleep(6)
+    type(quan_16x20)
+    quan_16x20.send_keys(sixteen_x_twenty)
+    time.sleep(13)
+    add3 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add3.click()
+    time.sleep(11)
+
+
+    browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Blank-Canvas-Panel-Set---12%22-x-24%22/p/80790634")
+    quan_12x24 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_12x24.clear()
+    time.sleep(8)
+    type(quan_12x24)
+    quan_12x24.send_keys(twelve_x_twentyfour)
+    time.sleep(9)
+    add4 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add4.click()
+    time.sleep(8)
+
+    browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Blank-Canvas-Panel-Set---18%22-x-24%22/p/7729")
+    quan_18x24 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_18x24.clear()
+    time.sleep(4)
+    type(quan_18x24)
+    quan_18x24.send_keys(eighteen_x_twentyfour)
+    time.sleep(11)
+    add5 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add5.click()
+    time.sleep(7)
+
+    browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Grandeur-Collection-Blank-Canvas---24%22-x-36%22/p/80936695")
+    quan_24x36 = browser.find_element(By.XPATH, '//*[@id="qty"]')
+    quan_24x36.clear()
+    time.sleep(7)
+    type(quan_24x36)
+    quan_24x36.send_keys(twentyfour_x_thirtysix)
+    time.sleep(13)
+    add6 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
+    add6.click()
+    time.sleep(12)
+
+    cart = browser.find_element(By.XPATH, '//*[@id="page"]/div/div[1]/div[3]/ul/li[5]/a[1]/span[1]')
+    cart.click()
+    time.sleep(25)
+
+    
+    browser.close()
+
+
 
 if __name__ == '__main__':
     main()
