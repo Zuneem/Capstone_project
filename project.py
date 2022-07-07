c4b37d986688bdbc253697f8ba567f3e1e10fd06
zuneem tamrakar <zuneemtamrakar@gmail.com>
2022-07-07 17:45:02 -0500

    added random sleep time between 1 - 3 secs

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ import time
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import random
 
 
 def main():
@@ -81,7 +82,7 @@ def place_order(new_dict):
     # print(eighteen_x_twentyfour)
     # print(twentyfour_x_thirtysix)
     
-
+    t = random.randint(1,3)
     browser = wd.Chrome('/Users/zuneemtamrakar/bin/chromedriver')
     browser.implicitly_wait(20)
 
@@ -90,13 +91,13 @@ def place_order(new_dict):
         browser.implicitly_wait(25)
         quan_11x14 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_11x14.clear()
-        time.sleep(5)
+        time.sleep(t)
         type(quan_11x14)
         quan_11x14.send_keys(eleven_x_fourteen)
-        time.sleep(13)
+        time.sleep(t)
         add1 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add1.click()
-        time.sleep(8)
+        time.sleep(t)
     except NoSuchElementException:
         print('11x14 is out of stock')
         pass
@@ -105,13 +106,13 @@ def place_order(new_dict):
         browser.get('https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Super-Value-Blank-Canvas-Set---8%22-x-10%22/p/80872876')
         quan_8x10 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_8x10.clear()
-        time.sleep(4)
+        time.sleep(t)
         type(quan_8x10)
         quan_8x10.send_keys(eight_x_ten)
-        time.sleep(9)
+        time.sleep(t)
         add2 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add2.click()
-        time.sleep(7)
+        time.sleep(t)
     except NoSuchElementException:
         print('8x10 is out of stock')
         pass
@@ -120,13 +121,13 @@ def place_order(new_dict):
         browser.get('https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Super-Value-Blank-Canvas-Set---16%22-x-20%22/p/80872705')
         quan_16x20 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_16x20.clear()
-        time.sleep(6)
+        time.sleep(t)
         type(quan_16x20)
         quan_16x20.send_keys(sixteen_x_twenty)
-        time.sleep(13)
+        time.sleep(t)
         add3 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add3.click()
-        time.sleep(11)
+        time.sleep(t)
     except NoSuchElementException:
         print('16x20 is out of stock')
         pass
@@ -135,13 +136,13 @@ def place_order(new_dict):
         browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Blank-Canvas-Panel-Set---12%22-x-24%22/p/80790634")
         quan_12x24 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_12x24.clear()
-        time.sleep(8)
+        time.sleep(t)
         type(quan_12x24)
         quan_12x24.send_keys(twelve_x_twentyfour)
-        time.sleep(9)
+        time.sleep(t)
         add4 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add4.click()
-        time.sleep(8)
+        time.sleep(t)
     except NoSuchElementException:
         print('12x24 is out of stock')
         pass
@@ -150,13 +151,13 @@ def place_order(new_dict):
         browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Blank-Canvas-Panel-Set---18%22-x-24%22/p/7729")
         quan_18x24 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_18x24.clear()
-        time.sleep(4)
+        time.sleep(t)
         type(quan_18x24)
         quan_18x24.send_keys(eighteen_x_twentyfour)
-        time.sleep(11)
+        time.sleep(t)
         add5 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add5.click()
-        time.sleep(7)
+        time.sleep(t)
     except NoSuchElementException:
         print('18x24 is out of stock')
         pass
@@ -165,17 +166,18 @@ def place_order(new_dict):
         browser.get("https://www.hobbylobby.com/Art-Supplies/Painting-Canvas-Art-Surfaces/Blank-Canvas/Master's-Touch-Grandeur-Collection-Blank-Canvas---24%22-x-36%22/p/80936695")
         quan_24x36 = browser.find_element(By.XPATH, '//*[@id="qty"]')
         quan_24x36.clear()
-        time.sleep(7)
+        time.sleep(t)
         type(quan_24x36)
         quan_24x36.send_keys(twentyfour_x_thirtysix)
-        time.sleep(13)
+        time.sleep(t)
         add6 = browser.find_element(By.XPATH, '//*[@id="addToCartButton"]/span')
         add6.click()
-        time.sleep(12)
+        time.sleep(8)
     except NoSuchElementException:
         print('24x36 is out of stock')
         pass
-    cart = browser.find_element(By.XPATH, '//*[@id="page"]/div/div[1]/div[3]/ul/li[5]/a[1]/span[1]')
+
+    cart = browser.find_element(By.XPATH, '//*[@id="page"]/div/div[1]/div[3]/ul/li[5]/a[1]')
     cart.click()
     time.sleep(25)
 
